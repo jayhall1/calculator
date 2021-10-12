61675eb86d3d9a2db6aaa43dc4c08237d71e05da
jaylen  hall <jaylenhall1@gmail.com>
2021-10-12 17:05:46 -0500

    finished functions commit

--- calc.js
+++ calc.js
@@ -5,17 +5,31 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
-
 reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
-	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+    tokens = input.split(' ');
+    
+    mathSymbol = tokens[0];
+    num1 = Number(tokens[1]);
+    num2 = Number(tokens[2]);
+
+    console.log('mathSymbol', mathSymbol);
+    console.log('num1', num1);
+    console.log('num2', num2);
+
+    if(mathSymbol === "+") {
+        console.log(`Answer: ${num1 + num2}`)
+    } else if(mathSymbol === "-") {
+        console.log(`Answer: ${num1 - num2}`)
+    } else if(mathSymbol === "*") {
+        console.log(`Answer: ${num1 * num2}`)
+    } else if(mathSymbol === "/") {
+        console.log(`Answer: ${num1 / num2}`)
+    } else if(mathSymbol === "sqrt") {
+        console.log(`Answer: ${Math.sqrt(num1)}`)
+    
+    };
 
 
-	// This line closes the connection to the command line interface.
-	reader.close()
+reader.close()
 
 });
